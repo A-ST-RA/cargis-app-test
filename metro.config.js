1a82f51dd7248e11a3fbb3dd8c85588d79d4745d
astra <astrax200469@gmail.com>
2023-09-22 00:43:16 +0300

    feat(architecture): create of base acrhitecture
    Added base folder structure
    Sass integration wip

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,18 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = (async () => {
+  const {
+    resolver: {sourceExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-sass-transformer'),
+    },
+    resolver: {
+      sourceExts: [...sourceExts, 'scss', 'sass'],
+    },
+  };
+})();
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
